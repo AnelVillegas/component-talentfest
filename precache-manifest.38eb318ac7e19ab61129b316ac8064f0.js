e25a85817ff0f629d227e18103c40e5728407060
Hanhelle <hanhelle@gmail.com>
2019-08-05 09:49:03 -0500

    Updates

---
+++ precache-manifest.38eb318ac7e19ab61129b316ac8064f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b384fce7ef5bed86107e0c4c38a9afec",
+    "url": "/component-talntfest/index.html"
+  },
+  {
+    "revision": "67a46a23195994600268",
+    "url": "/component-talntfest/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "6a8f280a5b751545b21b",
+    "url": "/component-talntfest/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "67a46a23195994600268",
+    "url": "/component-talntfest/static/js/2.1781a99f.chunk.js"
+  },
+  {
+    "revision": "6a8f280a5b751545b21b",
+    "url": "/component-talntfest/static/js/main.9bfaf32c.chunk.js"
+  },
+  {
+    "revision": "5ec1b7075bf7159eaf71",
+    "url": "/component-talntfest/static/js/runtime~main.8420fed0.js"
+  }
+]);
